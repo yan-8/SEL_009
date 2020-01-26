10198f5885c19ae240de25f6cdb666169b3170ca
Yan <yan.bondarenko@gmail.com>
2020-01-27 00:18:46 +0200

    Add driver and created init test

--- src/test/java/com/automationpractice/BaseTest.java
+++ src/test/java/com/automationpractice/BaseTest.java
@@ -3,17 +3,20 @@ package com.automationpractice;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
     private WebDriver driver;
 
     @Before
     public void start() {
-
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/mac/chrome/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://automationpractice.com");
     }
 
     @After
     public void end() {
-
+        driver.quit();
     }
 }
---
+++ src/test/java/com/automationpractice/Tests.java
@@ -0,0 +1,10 @@
+package com.automationpractice;
+
+import org.junit.Test;
+
+public class Tests extends BaseTest {
+    @Test
+    public void test1() {
+
+    }
+}
