d1bb6f8bd287a0078d4bb1d9b9626cbb3b3d7df2
Yan <yan.bondarenko@gmail.com>
2020-02-06 23:09:26 +0200

    Add Gecko and Safari browsers

--- src/test/java/com/automationpractice/manager/ApplicationManager.java
+++ src/test/java/com/automationpractice/manager/ApplicationManager.java
@@ -4,8 +4,11 @@ import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.plugin2.util.BrowserType;
 
 public class ApplicationManager {
     private WebDriver driver;
@@ -13,6 +16,11 @@ public class ApplicationManager {
     private Faker faker;
     private LoginHelper loginHelper;
     private SignUpHelper signUpHelper;
+    private int browser;
+
+    public ApplicationManager(int browser) {
+        this.browser = browser;
+    }
 
     public LoginHelper getLoginHelper() {
         return loginHelper;
@@ -23,8 +31,18 @@ public class ApplicationManager {
     }
 
     public void initBrowser() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/mac/chrome/chromedriver");
-        driver = new ChromeDriver();
+        if (browser == BrowserType.DEFAULT) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/mac/chrome/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser == BrowserType.MOZILLA) {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/mac/gecko/geckodriver");
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserType.SAFARI_MACOSX) {
+            driver = new SafariDriver();
+        } else {
+            System.out.println("NO BROWSER");
+        }
+
         wait = new WebDriverWait(driver, 15); // явное
         faker = new Faker();
 
--- src/test/java/com/automationpractice/tests/BaseTest.java
+++ src/test/java/com/automationpractice/tests/BaseTest.java
@@ -3,9 +3,10 @@ package com.automationpractice.tests;
 import com.automationpractice.manager.ApplicationManager;
 import org.junit.After;
 import org.junit.Before;
+import sun.plugin2.util.BrowserType;
 
 public class BaseTest {
-    protected final ApplicationManager automationPracticeApp = new ApplicationManager();
+    protected final ApplicationManager automationPracticeApp = new ApplicationManager(BrowserType.MOZILLA);
 
     @Before
     public void start() {
